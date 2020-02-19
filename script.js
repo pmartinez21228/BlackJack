ec362baa757195ee980902b1d3f110bbbd5fee5d
Pablo Martinez <21228@columbushs.com>
2020-02-19 12:53:39 -0500

    working on hit and stand

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 let deck = [
-    { image: "url('cards/2C.png')", isPlayed: false, value: 2 },
-    { image: "url('cards/2D.png')", isPlayed: false, value: 2 },
-    { image: "url('cards/2H.png')", isPlayed: false, value: 2 },
-    { image: "url('cards/2S.png')", isPlayed: false, value: 2 },
-    { image: "url('cards/3C.png')", isPlayed: false, value: 3 },
-    { image: "url('cards/3D.png')", isPlayed: false, value: 3 },
-    { image: "url('cards/3H.png')", isPlayed: false, value: 3 },
-    { image: "url('cards/3S.png')", isPlayed: false, value: 3 },
-    { image: "url('cards/4C.png')", isPlayed: false, value: 4 },
-    { image: "url('cards/4D.png')", isPlayed: false, value: 4 },
-    { image: "url('cards/4H.png')", isPlayed: false, value: 4 },
-    { image: "url('cards/4S.png')", isPlayed: false, value: 4 },
-    { image: "url('cards/5C.png')", isPlayed: false, value: 5 },
-    { image: "url('cards/5D.png')", isPlayed: false, value: 5 },
-    { image: "url('cards/5H.png')", isPlayed: false, value: 5 },
-    { image: "url('cards/5S.png')", isPlayed: false, value: 5 },
-    { image: "url('cards/6C.png')", isPlayed: false, value: 6 },
-    { image: "url('cards/6D.png')", isPlayed: false, value: 6 },
-    { image: "url('cards/6H.png')", isPlayed: false, value: 6 },
-    { image: "url('cards/6S.png')", isPlayed: false, value: 6 },
-    { image: "url('cards/7C.png')", isPlayed: false, value: 7 },
-    { image: "url('cards/7D.png')", isPlayed: false, value: 7 },
-    { image: "url('cards/7H.png')", isPlayed: false, value: 7 },
-    { image: "url('cards/7S.png')", isPlayed: false, value: 7 },
-    { image: "url('cards/8C.png')", isPlayed: false, value: 8 },
-    { image: "url('cards/8D.png')", isPlayed: false, value: 8 },
-    { image: "url('cards/8H.png')", isPlayed: false, value: 8 },
-    { image: "url('cards/8S.png')", isPlayed: false, value: 8 },
-    { image: "url('cards/9C.png')", isPlayed: false, value: 9 },
-    { image: "url('cards/9D.png')", isPlayed: false, value: 9 },
-    { image: "url('cards/9H.png')", isPlayed: false, value: 9 },
-    { image: "url('cards/9S.png')", isPlayed: false, value: 9 },
-    { image: "url('cards/10C.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/10D.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/10H.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/10S.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/JC.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/JD.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/JH.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/JS.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/QC.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/QD.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/QH.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/QS.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/KC.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/KD.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/KH.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/KS.png')", isPlayed: false, value: 10 },
-    { image: "url('cards/AC.png')", isPlayed: false, value: 11 },
-    { image: "url('cards/AD.png')", isPlayed: false, value: 11 },
-    { image: "url('cards/AH.png')", isPlayed: false, value: 11 },
-    { image: "url('cards/AS.png')", isPlayed: false, value: 11 },
+    { image: "url('cards/2C.png')", value: 2 },
+    { image: "url('cards/2D.png')", value: 2 },
+    { image: "url('cards/2H.png')", value: 2 },
+    { image: "url('cards/2S.png')", value: 2 },
+    { image: "url('cards/3C.png')", value: 3 },
+    { image: "url('cards/3D.png')", value: 3 },
+    { image: "url('cards/3H.png')", value: 3 },
+    { image: "url('cards/3S.png')", value: 3 },
+    { image: "url('cards/4C.png')", value: 4 },
+    { image: "url('cards/4D.png')", value: 4 },
+    { image: "url('cards/4H.png')", value: 4 },
+    { image: "url('cards/4S.png')", value: 4 },
+    { image: "url('cards/5C.png')", value: 5 },
+    { image: "url('cards/5D.png')", value: 5 },
+    { image: "url('cards/5H.png')", value: 5 },
+    { image: "url('cards/5S.png')", value: 5 },
+    { image: "url('cards/6C.png')", value: 6 },
+    { image: "url('cards/6D.png')", value: 6 },
+    { image: "url('cards/6H.png')", value: 6 },
+    { image: "url('cards/6S.png')", value: 6 },
+    { image: "url('cards/7C.png')", value: 7 },
+    { image: "url('cards/7D.png')", value: 7 },
+    { image: "url('cards/7H.png')", value: 7 },
+    { image: "url('cards/7S.png')", value: 7 },
+    { image: "url('cards/8C.png')", value: 8 },
+    { image: "url('cards/8D.png')", value: 8 },
+    { image: "url('cards/8H.png')", value: 8 },
+    { image: "url('cards/8S.png')", value: 8 },
+    { image: "url('cards/9C.png')", value: 9 },
+    { image: "url('cards/9D.png')", value: 9 },
+    { image: "url('cards/9H.png')", value: 9 },
+    { image: "url('cards/9S.png')", value: 9 },
+    { image: "url('cards/10C.png')", value: 10 },
+    { image: "url('cards/10D.png')", value: 10 },
+    { image: "url('cards/10H.png')", value: 10 },
+    { image: "url('cards/10S.png')", value: 10 },
+    { image: "url('cards/JC.png')", value: 10 },
+    { image: "url('cards/JD.png')", value: 10 },
+    { image: "url('cards/JH.png')", value: 10 },
+    { image: "url('cards/JS.png')", value: 10 },
+    { image: "url('cards/QC.png')", value: 10 },
+    { image: "url('cards/QD.png')", value: 10 },
+    { image: "url('cards/QH.png')", value: 10 },
+    { image: "url('cards/QS.png')", value: 10 },
+    { image: "url('cards/KC.png')", value: 10 },
+    { image: "url('cards/KD.png')", value: 10 },
+    { image: "url('cards/KH.png')", value: 10 },
+    { image: "url('cards/KS.png')", value: 10 },
+    { image: "url('cards/AC.png')", value: 11 },
+    { image: "url('cards/AD.png')", value: 11 },
+    { image: "url('cards/AH.png')", value: 11 },
+    { image: "url('cards/AS.png')", value: 11 },
 ]
 let dealer1 = document.getElementById("card1");
 let dealer2 = document.getElementById("card2");
@@ -67,16 +67,17 @@ let chip10 = document.getElementById("10");
 let chip20 = document.getElementById("20");
 let chip50 = document.getElementById("50");
 let chip100 = document.getElementById("100");
+let leftDeck = document.getElementById("deck")
+let hit = document.getElementById("hit")
+let stand = document.getElementById("stand")
 let card = "url('cards/red_back.png')"
 let playerCount = 0;
 let dealerCount = 0;
 let rand = null;
 let isPlaying = false;
+let betReady = false;
 
 function restart(){
-    deck.forEach(function(x){
-        x.isPlayed = false;
-    })
     dealer1.style.backgroundImage = null;
     dealer2.style.backgroundImage = null;
     player1.style.backgroundImage = null;
@@ -85,11 +86,17 @@ function restart(){
     dealerCount = 0;
     credit = 1000;
     balance.innerHTML = credit;
+    isPlaying = false;
+    activeBet = 0;
+    bet.innerHTML = activeBet;
+    betReady = false;
 }
 function randNum() {
     rand = Math.floor(Math.random() * 52)
 }
 function displayBack() {
+    playerCount = 0
+    dealerCount = 0
     setTimeout(function () {
         dealer1.style.backgroundImage = card
     }, 500)
@@ -102,68 +109,69 @@ function displayBack() {
     setTimeout(function () {
         player2.style.backgroundImage = card
     }, 2000)
+    betReady = true;
 }
 function displayFront() {
     setTimeout(function () {
         randNum()
-        if (!deck[rand].isPlayed) {
             dealer1.style.backgroundImage = deck[rand].image
             dealerCount = dealerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        } else {
-            while (deck[rand].isPlayed) {
-                randNum()
-            }
-            dealer1.style.backgroundImage = deck[rand].image
-            dealerCount = dealerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        }
-
     }, 2500)
     setTimeout(function () {
         randNum()
-        if (!deck[rand].isPlayed) {
             player1.style.backgroundImage = deck[rand].image
             playerCount = playerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        } else {
-            while (deck[rand].isPlayed) {
-                randNum()
-            }
-            player1.style.backgroundImage = deck[rand].image
-            playerCount = playerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        }
-
     }, 3000)
     setTimeout(function () {
         randNum()
-        if (!deck[rand].isPlayed) {
             player2.style.backgroundImage = deck[rand].image
             playerCount = playerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        } else {
-            while (deck[rand].isPlayed) {
-                randNum()
-            }
-            player2.style.backgroundImage = deck[rand].image
-            playerCount = playerCount + deck[rand].value
-            deck[rand].isPlayed = true;
-        }
-
     }, 3500)
 }
 function startGame() {
     displayBack()
-    setTimeout(displayFront(), 2500)
+    //setTimeout(displayFront(),2500)
+    isPlaying = true;
+}
+function setBet(x){
+    activeBet = Number(activeBet) + x;
+    bet.innerHTML = activeBet
+    credit = Number(credit) - x;
+    balance.innerHTML = credit;
 }
 start.onclick = function () {
     if (!isPlaying) {
         startGame()
-        isPlaying = true;
     }
 }
 
 reset.onclick = function(){
     restart()
+}
+chip10.onclick = function(){
+    if(betReady && credit - 10 >= 0){
+        setBet(10);
+    }
+}
+chip20.onclick = function(){
+    if(betReady && credit - 20 >= 0){
+        setBet(20);
+    }
+}
+chip50.onclick = function(){
+    if(betReady && credit - 50 >= 0){
+        setBet(50);
+    }
+}
+chip100.onclick = function(){
+    if(betReady && credit - 100 >= 0){
+        setBet(100);
+    }
+}
+leftDeck.onclick = function(){
+    if(isPlaying && betReady){
+        displayFront()
+        betReady = false;
+    }
+
 }
